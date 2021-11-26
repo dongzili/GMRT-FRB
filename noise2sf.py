scaling fix
- major flaw in re-digitizing logic where there was no rescaling step, whitened data was directly digitized without rescaling
- had redundant logic in noise2sf which caused coherence->stokes->coherence computation

noise2sf.py
@@ -215,17 +215,11 @@ if __name__ == "__main__":
     #################################
     ## work loop
     isubint = 0
-    udat     = np.zeros ((hGULP, nch, npl), dtype=np.int16)
-    vdat     = np.zeros ((hGULP, nch, npl), dtype=np.int16)
     rdat     = np.zeros ((GULP, nch, npl), dtype=np.int16)
     sdat     = np.zeros ((GULP, nch, npl), dtype=np.int16)
-    pdat     = np.zeros ((nch, npl, GULP), dtype=np.uint16)
     # slices
 
     for i in tr:
-        udat[:] = 0
-        vdat[:] = 0
-        pdat[:] = 0
         rdat[:] = 0
         sdat[:] = 0
         ### reading
@@ -241,22 +235,9 @@ if __name__ == "__main__":
         dat shape = (nchans, npol  , nsamps)
         """
         ###
-        ## GMRT pol order to full stokes IQUV
-        ## may need to complicate to support total intensity
-        udat[...,0] = pkg_on[...,0] + pkg_on[...,2]
-        udat[...,1] = pkg_on[...,1]
-        udat[...,2] = pkg_on[...,3]
-        udat[...,3] = pkg_on[...,0] - pkg_on[...,2]
-
-        vdat[...,0] = pkg_of[...,0] + pkg_of[...,2]
-        vdat[...,1] = pkg_of[...,1]
-        vdat[...,2] = pkg_of[...,3]
-        vdat[...,3] = pkg_of[...,0] - pkg_of[...,2]
-
-        ###
         ## OFF-ON-OFF
-        rdat[:hGULP] = udat[:]
-        rdat[hGULP:] = vdat[:]
+        rdat[:hGULP] = pkg_on[:]
+        rdat[hGULP:] = pkg_of[:]
         sdat[:]     = np.roll (rdat, 768, axis=0)
 
         ###

redigitize.py
@@ -25,6 +25,13 @@ class Redigitize:
         ran             = iinfo.max - iinfo.min
         self.omin       = - ran // 2
         self.omax       = ran // 2
+        self.ofac       = None
+        if odtype == np.uint8:
+            self.ofac   = 40
+        elif odtype == np.uint16:
+            self.ofac   = 10880
+        if self.ofac is None:
+            raise ValueError ("unable to set ofac")
 
         self.gulp       = gulp
         self.nchans     = nchans
@@ -65,10 +72,21 @@ class Redigitize:
         ## stokes computation
         ## see https://arxiv.org/pdf/2004.08542.pdf
         ## XXX RR, RL*, LL, R*L
-        self.sdat[...,0] = gfb[...,0] + gfb[...,2]
-        self.sdat[...,1] = -gfb[...,1] - gfb[...,3]
-        self.sdat[...,2] = gfb[...,1] - gfb[...,3]
-        self.sdat[...,3] = gfb[...,2] - gfb[...,0]
+        #self.sdat[...,0] = gfb[...,0] + gfb[...,2]
+        #self.sdat[...,1] = -gfb[...,1] - gfb[...,3]
+        #self.sdat[...,2] = gfb[...,1] - gfb[...,3]
+        #self.sdat[...,3] = gfb[...,2] - gfb[...,0]
+        ## 2021-11-24 SB: see discussion in POL_TYPE in obsinfo
+        ## XXX AABBCRCI === RR,LL,RL*,R*L
+        self.sdat[...,0] = gfb[...,0]
+        self.sdat[...,1] = gfb[...,2]
+        self.sdat[...,2] = gfb[...,1]
+        self.sdat[...,3] = gfb[...,3]
+        ## POL_TYPE = "IVQU" circular basis
+        #self.sdat[...,0] = gfb[...,0] + gfb[...,2]
+        #self.sdat[...,1] = gfb[...,2] - gfb[...,0]
+        #self.sdat[...,2] = -gfb[...,1] - gfb[...,3]
+        #self.sdat[...,3] = gfb[...,1] - gfb[...,3]
 
 
         ##
@@ -95,12 +113,13 @@ class Redigitize:
 
         ##
         ## digitize
+        # XXX 20211126 - SB: i am stupid enough to not add the scaling step here
         #### sdat is (nsamps, nchans, npol)
         #### bdat is (nsamps, npol, nchans)
-        self.dat[:,0,:] = np.clip (self.sdat[...,0], self.omin, self.omax)+self.omin
-        self.dat[:,1,:] = np.clip (self.sdat[...,1], self.omin, self.omax)+self.omin
-        self.dat[:,2,:] = np.clip (self.sdat[...,2], self.omin, self.omax)+self.omin
-        self.dat[:,3,:] = np.clip (self.sdat[...,3], self.omin, self.omax)+self.omin
+        self.dat[:,0,:] = np.clip (self.ofac * self.sdat[...,0], self.omin, self.omax)+self.omin
+        self.dat[:,1,:] = np.clip (self.ofac * self.sdat[...,1], self.omin, self.omax)+self.omin
+        self.dat[:,2,:] = np.clip (self.ofac * self.sdat[...,2], self.omin, self.omax)+self.omin
+        self.dat[:,3,:] = np.clip (self.ofac * self.sdat[...,3], self.omin, self.omax)+self.omin
 
 
 if __name__ == "__main__":