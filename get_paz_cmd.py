ecc38cf9a7b150cb4d84ce6e0973f3a0c4bad25d
shiningsurya <15906282+shiningsurya@users.noreply.github.com>
2025-07-28 11:56:08 +0200

    clean

--- get_paz_cmd.py
+++ get_paz_cmd.py
@@ -1,9 +1,19 @@
 # coding: utf-8
 import os
+<<<<<<< HEAD
 import subprocess as sp
 import numpy as np
 
 CMD="psrstat -Q -jT -c int:wt {ar}"
+=======
+import numpy as np
+import astropy.io.fits as aif
+
+def find_table ( names, tab ):
+    for i,n in enumerate ( names ):
+        if n == tab:
+            return i
+    raise RuntimeError (f"No {tab} found")
 
 def get_args ():
     import argparse as agp
@@ -27,6 +37,36 @@ def action (ar, og):
     ZZZ = "paz {og} -z ".format(og=" ".join(og)) + ",".join(map(str,zz)) + " " + ar
     print ( ZZZ )
 
+    # add ('-v', '--verbose', help='Verbose', dest='v', action='store_true')
+    return ap.parse_args ()
+
+def action ( ar):
+    """ input file """
+    ##
+    bn = os.path.basename ( ar )
+    tag = bn [ :bn.find('_R3') ] + "_R3"
+
+    ##
+    f = aif.open ( ar )
+    fpri = f[0]
+
+
+    ## get sub
+    SUBIDX = find_table ( [fi.name for fi in f], 'SUBINT' )
+    fsub   = f[SUBIDX]
+
+    ## get DAT_WTS
+    dw     = fsub.data['DAT_WTS'][0]
+    ww     = np.where ( dw == 0. )[0]
+
+    kill_list = ""
+    for ik in ww:
+        kill_list += f"{ik:d},"
+    kill_list = kill_list[:-1]
+
+    CMD    = "paz -z \"" + kill_list + f"\" {tag}.PR -e PRz"
+
+    print (CMD)
 
 
     ##
@@ -35,6 +75,7 @@ if __name__ == "__main__":
     args = get_args ()
     for a in args.ar:
         action ( a, og=args.og )
+        action ( a )
 ######################################################
 
 
