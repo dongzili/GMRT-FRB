a2d3c5b5c0c3c31dc96bd4f9df948152769aaac0
shiningsurya <15906282+shiningsurya@users.noreply.github.com>
2025-08-14 19:33:10 +0200

    spinifex ionospheric RM

--- get_ionos_rm.py
+++ get_ionos_rm.py
@@ -1,61 +1,47 @@
 """
 measure the ionospheric RM at the time of input
+using spinifex
 """
 # coding: utf-8
 import os
 import numpy as np
 import pandas as pd
 
-import astroplan as ap
-
 import astropy.time as at
 import astropy.coordinates as asc
 import astropy.units as au
 import astropy.io.fits as aif
+
+from spinifex import get_rm
+
+SPINIFEX_DIR = "/tmp/temp_spinifex"
 ################################################
 gmrt = asc.EarthLocation (
         lat='19d06m',
         lon='74d03m',
         height=300, 
 )
-observer = ap.Observer (location=gmrt)
 
 RMS   = {"0329+54":-64.33, "0139+5814":-94.13, "R3":np.nan}
 
-def get_parallactic_angle ( ra, dec, mjd ):
+def get_parallactic_angle ( sc, tobs ):
     """ source coordinates, mjd --> parallactic angle (degree) """
-    atm  = at.Time ( mjd, format='mjd' )
-    atc  = asc.SkyCoord ( ra, dec, unit=(au.hourangle, au.degree)  )
-    pal  = observer.parallactic_angle (atm, atc).to(au.degree).value
-    return pal
 
-def get_ionospheric_rm ( ra, dec, mjd, duration=30., nsteps=2, prefix='uqrg' ):
-    """ uses RMextract """
-    from RMextract.getRM import getRM
-    ###
-    pointing = []
-    if isinstance(ra, str) and isinstance(dec, str):
-        atc      = asc.SkyCoord ( ra, dec, unit=(au.hourangle, au.degree)  )
-        pointing = [ atc.ra.radian, atc.dec.radian ]
-    elif isinstance ( ra, float ) and isinstance ( dec, float ):
-        pointing = [ ra, dec ]
-    else:
-        raise ValueError("pointing error")
-    gmrt_pos = [ 1656342.30, 5797947.77, 2073243.16 ]
-    time     = mjd * 86400.0
-    ###
-    ret      = getRM ( 
-        radec=pointing,
-        stat_positions=[gmrt_pos],
-        timestep=nsteps,
-        timerange=[time,time+duration],
-        prefix=prefix,
-        # server="http://cddis.gsfc.nasa.gov/archive/gnss/products/ionex"
-        server="ftp://gssc.esa.int/gnss/products/ionex/"
+    lst   = tobs.sidereal_time ( 'mean', longitude=gmrt.lon, model=None )
+    h     = (lst - sc.ra).radian
+    q     = np.arctan2 ( 
+            np.sin ( h ), 
+            np.tan ( gmrt.lat.radian ) * np.cos ( sc.dec.radian ) - 
+            np.sin ( sc.dec.radian ) * np.cos ( h )
     )
-    ###
-    retrm    = ret['RM']['st1'].mean()
-    return retrm
+    return q
+
+def get_ionospheric_rm ( sc, tobs ):
+    """
+    wrapper
+    """
+    rm  = get_rm.get_rm_from_skycoord ( loc=gmrt, times=tobs, source=sc, output_directory=SPINIFEX_DIR, prefix="uqr", server='cddis' )
+    return rm
 
 def action ( ar ):
     cal           = aif.open ( ar )
@@ -72,22 +58,23 @@ def action ( ar ):
     stt_smjd  = ptab.header['STT_SMJD']
     stt_offs  = ptab.header['STT_OFFS']
     mjd       = ( stt_imjd ) + ( stt_smjd / 86400. ) + ( stt_offs / 86400 )
-    if args.v:
-        print (f" RA/DEC = {ra},{dec}")
+    ####################
+    tobs      = at.Time ( mjd, format='mjd' )
+    sc        = asc.SkyCoord ( ra, dec, unit=(au.hourangle, au.degree)  )
+    #### 
+    taxis     = tobs + ( np.array ([-60, -30, 0, 30, 60]) * au.min )
     ## get PAL
-    pal_deg   = get_parallactic_angle ( ra, dec, mjd )
-    ## get iRM
-    # ionos_rm  = get_ionospheric_rm ( ra, dec, mjd, prefix='upcg' )
-    ionos_rm  = get_ionospheric_rm ( ra, dec, mjd, prefix='uqrg' )
-    # ionos_rm  = get_ionospheric_rm ( ra, dec, mjd, prefix='cgim' )
+    pal       = get_parallactic_angle ( sc, tobs ) * au.radian
+    irm       = get_ionospheric_rm  ( sc, taxis )
+    ionos_rm = irm.rm[2]
     print (f" {ar}")
-    print (f"\tSource={src_name} at MJD={mjd:.6f} parallactic_angle={pal_deg:.2f} deg")
+    print (f"\tSource={src_name} at MJD={mjd:.6f} parallactic_angle={pal.to(au.degree):.2f}")
     print (f"\tIonospheric RM = {ionos_rm:.3f}")
-    corr_rm   = ionos_rm + RMS[src_name]
-    print (f"\tCorrection RM = {corr_rm:.3f}")
+    if src_name in RMS.keys():
+        corr_rm   = ionos_rm + RMS[src_name]
+        print (f"\tCorrection RM = {corr_rm:.3f}")
 
 ##############################
-SOL = 'pacv_sols/3C48_NGON_bm3_pa_550_200_32_16mar2021.raw.noise.ar.pazi.pacv'
 def get_args ():
     import argparse as agp
     ap   = agp.ArgumentParser ('get_ionos_rm', description='Measures ionospheric RM contribution', epilog='Part of GMRT/FRB')
@@ -103,6 +90,7 @@ if __name__ == "__main__":
     # read file
     for a in args.ar:
         action ( a )
+
 
 
 
